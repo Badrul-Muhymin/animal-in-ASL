995e72790a155d1faceadf7e6c94e0ce801fd289
Badrul-Muhymin <115321135+Badrul-Muhymin@users.noreply.github.com>
2024-01-26 16:40:17 +0800

    added asset from nuha

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    //home: Text('Hi Newbie'),
     home: Scaffold(
       appBar: AppBar(
         title: const Text('Nuha nak Apps background anime'),
         centerTitle: true,
         backgroundColor: Colors.blue,
       ),
-      body:Image.asset(
-                  'assets/bg.jpg', // Replace with your image path
-                  fit: BoxFit.cover,
-                  width: double.infinity,
-                  height: double.infinity,
-
+      body:
+      Image.asset(
+        'assets/Background1.jpeg', // Replace with your image path
+        fit: BoxFit.cover,
+        width: double.infinity,
+        height: double.infinity,
     ),
   )),
   );
